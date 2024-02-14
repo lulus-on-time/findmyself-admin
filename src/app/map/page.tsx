4b7f9b306cbc762ced4bbf44faf9b5a6e0fe85d3
melisaou <melisaangelina08@gmail.com>
2024-02-14 18:17:18 +0700

    test: Use GeoJSON

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -10,49 +10,109 @@ const ManualMap = () => {
   useEffect(() => {
     if (!mapRef.current) return;
 
-    // Check if map instance already exists
     // @ts-ignore
     if (!mapRef.current._leaflet_id) {
       var map = L.map("map", {
         crs: L.CRS.Simple,
         minZoom: -2,
-        maxZoom: 1,
+        maxZoom: 2,
       });
 
       var bounds = [
         [0, 0],
         [577, 1204],
       ];
+
       // @ts-ignore
-      var image = L.imageOverlay("/images/maps.png", bounds).addTo(map);
+      var image = L.imageOverlay("/images/maps2.png", bounds).addTo(map);
       // @ts-ignore
       map.fitBounds(bounds);
 
-      var marker = L.marker([577 - 100, 150], {
-        icon: new Icon({
-          iconUrl: "/images/marker.svg",
-          iconSize: [24, 24],
-          iconAnchor: [12, 12],
-        }),
+      var roomGeoJSON = {
+        type: "FeatureCollection",
+        features: [
+          {
+            type: "Feature",
+            properties: { name: "Auditorium" },
+            geometry: {
+              type: "Point",
+              coordinates: [150, 577 - 125],
+            },
+          },
+          {
+            type: "Feature",
+            properties: { name: "Lab Komputer A3.01" },
+            geometry: {
+              type: "Point",
+              coordinates: [400, 577 - 100],
+            },
+          },
+          {
+            type: "Feature",
+            properties: { name: "Kelas A3.05" },
+            geometry: {
+              type: "Point",
+              coordinates: [850, 577 - 440],
+            },
+          },
+        ],
+      };
+
+      var labelIcon = (roomName: string) =>
+        new L.DivIcon({
+          className: "my-div-icon",
+          html: `<div style="display:flex; gap:4px; align-items:start"><img alt="marker" src="/images/marker.svg" /><span>${roomName}</span></div>`,
+          iconAnchor: [8, 12],
+        });
+
+      var roomIcon = L.icon({
+        iconUrl: "/images/marker.svg",
+        iconSize: [24, 24],
+        iconAnchor: [12, 12],
+        tooltipAnchor: [12, 0],
+      });
+
+      // @ts-ignore
+      L.geoJSON(roomGeoJSON, {
+        pointToLayer: function (feature, position) {
+          return L.marker(position, {
+            icon: labelIcon(feature.properties.name),
+            // icon: roomIcon,
+          });
+          // .bindTooltip(feature.properties.name, {
+          //   permanent: true,
+          //   direction: "right",
+          // });
+        },
       }).addTo(map);
 
-      var marker2 = L.marker([577 - 200, 300], {
-        icon: new Icon({
-          iconUrl: "/images/wifi.svg",
-          iconSize: [24, 24],
-          iconAnchor: [12, 12],
-        }),
-      }).addTo(map);
-      marker2.bindPopup("Hello, I'm access point 1").openPopup();
+      var myLocationGeoJSON = {
+        type: "FeatureCollection",
+        features: [
+          {
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: [150, 577 - 145],
+            },
+          },
+        ],
+      };
 
-      var marker3 = L.marker([577 - 100, 400], {
-        icon: new Icon({
-          iconUrl: "/images/wifi.svg",
-          iconSize: [24, 24],
-          iconAnchor: [12, 12],
-        }),
+      var accessPointIcon = L.icon({
+        iconUrl: "/images/my-loc.svg",
+        iconSize: [24, 24],
+        iconAnchor: [12, 12],
+      });
+
+      // @ts-ignore
+      L.geoJSON(myLocationGeoJSON, {
+        pointToLayer: function (feature, position) {
+          return L.marker(position, {
+            icon: accessPointIcon,
+          });
+        },
       }).addTo(map);
-      marker3.bindPopup("Hello, I'm access point 2").openPopup();
     }
   }, []);
 
@@ -60,7 +120,7 @@ const ManualMap = () => {
     <div className="w-full h-screen flex justify-center items-center bg-white text-black">
       <div
         id="map"
-        style={{ height: "577px", width: "1204px" }}
+        style={{ height: "577px", width: "1204px", background: "#F5F5F5" }}
         ref={mapRef}
       ></div>
     </div>
